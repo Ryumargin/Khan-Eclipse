e249e789c011557241e61ebd5da9990b5816710f
&𝚁𝚢𝚞𝙳𝚞𝚌𝚔 <yurijoga95@gmail.com>
2025-06-23 19:46:04 -0300

    Update KhanCheetus.js

--- KhanCheetus.js
+++ KhanCheetus.js
@@ -1,7 +1,7 @@
 const ver = "V3.1.1";
 let isDev = false;
 
-const repoPath = `https://raw.githubusercontent.com/Niximkk/Khanware/refs/heads/${isDev ? "dev/" : "main/"}`; // Keep original repo path for fetching
+const repoPath = `https://raw.githubusercontent.com/Ryumargin/KhanCheetus/refs/heads/${isDev ? "dev/" : "main/"}`; // Keep original repo path for fetching
 
 let device = {
     mobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|Mobile|Tablet|Kindle|Silk|PlayBook|BB10/i.test(navigator.userAgent),
