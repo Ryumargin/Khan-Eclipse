1e4ddf07c13dbb5e4645a7362151fe0c86545750
&𝚁𝚢𝚞𝙳𝚞𝚌𝚔 <yurijoga95@gmail.com>
2025-06-23 20:22:58 -0300

    Update mainMenu.js

--- visuals/mainMenu.js
+++ visuals/mainMenu.js
@@ -92,9 +92,9 @@ dropdownMenu.innerHTML = `
     <style>
         input[type="checkbox"] {appearance: none; width: 15px; height: 15px; background-color: #3a3a3b;
         border: 1px solid #acacac; border-radius: 3px; margin-right: 5px; cursor: pointer;}
-        input[type="checkbox"]:checked {background-color: #540b8a; border-color: #720fb8;}
+        input[type="checkbox"]:checked {background-color: FFA500; border-color: #720fb8;}
         input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 10px); border: 1px solid #343434; 
-        color: white; accent-color: #540b8a; background-color: #540b8a; padding: 3px; border-radius: 3px; background: none;}
+        color: white; accent-color: FFA500; background-color: FFA500; padding: 3px; border-radius: 3px; background: none;}
         label {display: flex; align-items: center; color: #3a3a3b; padding-top: 3px;}
     </style>
 `;
