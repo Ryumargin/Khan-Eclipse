2e49f305c1024049ea93496961d46d4acbb62e3e
&𝚁𝚢𝚞𝙳𝚞𝚌𝚔 <yurijoga95@gmail.com>
2025-06-23 21:02:51 -0300

    Add files via upload

---
+++ functions/customBanner.js
@@ -0,0 +1,14 @@
+const phrases = [ 
+    "[⭐] Non Skeetless dude.",
+    "[⭐] KhanCheetus on top.",
+    "[⭐] KhanCheetus said hello!",
+    "[⭐] God i wish i had KhanCheetus.",
+    "[⭐] Get good get KhanCheetus!",
+    "[⭐] KhanCheetus.space goes brrrrr" 
+];
+
+setInterval(() => { 
+    const greeting = document.querySelector('.stp-animated-banner h2');
+    if (greeting&&features.customBanner) greeting.textContent = phrases[Math.floor(Math.random() * phrases.length)];
+}, 3000);
+
