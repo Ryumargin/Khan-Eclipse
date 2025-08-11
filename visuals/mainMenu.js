72f609fce094659a3a8fba11f5ab46f2f3af9da2
𝙋-𝙘𝙝𝙖𝙣 𝙧𝙖𝙜 <yurijoga95@gmail.com>
2025-08-10 17:19:08 -0700

    Update mainMenu.js

--- visuals/mainMenu.js
+++ visuals/mainMenu.js
@@ -69,7 +69,7 @@ Object.assign(watermark.style, {
 
 if (device.mobile) watermark.style.left = '55%'
 
-watermark.innerHTML = `<span style="text-shadow: -1px 0.5px 0 #72ff72, -2px 0px 0 #2f672e;">KW</span> <span style="color:gray; padding-left:2px; font-family: Arial, sans-serif; font-size:10px">${ver}</span>`;
+watermark.innerHTML = `<span style="text-shadow: -1px 0.5px 0 #ffa435ff, -2px 0px 0 #FF8C00;">PChan</span> <span style="color:gray; padding-left:2px; font-family: Arial, sans-serif; font-size:10px">${ver}</span>`;
 
 document.body.appendChild(watermark);
 
@@ -92,9 +92,9 @@ dropdownMenu.innerHTML = `
     <style>
         input[type="checkbox"] {appearance: none; width: 15px; height: 15px; background-color: #3a3a3b;
         border: 1px solid #acacac; border-radius: 3px; margin-right: 5px; cursor: pointer;}
-        input[type="checkbox"]:checked {background-color: #540b8a; border-color: #720fb8;}
+        input[type="checkbox"]:checked {background-color: #FF8C00; border-color: #FF8C00;}
         input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 10px); border: 1px solid #343434; 
-        color: white; accent-color: #540b8a; background-color: #540b8a; padding: 3px; border-radius: 3px; background: none;}
+        color: white; accent-color: #FF8C00; background-color: #FF8C00; padding: 3px; border-radius: 3px; background: none;}
         label {display: flex; align-items: center; color: #3a3a3b; padding-top: 3px;}
     </style>
 `;
